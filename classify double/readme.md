1504145f6eb1272cc842f7d4a10d0897d1fc9c95
Konstantin Leladze <leladze.kg@phystech.edu>
2020-09-15 13:38:55 +0300

    added classify double task

---
+++ classify double/readme.md
@@ -0,0 +1,8 @@
+# Задача 1: classify double
+
+### Условие:
+Необходимо написать checker-ы для функции classify (double num) {...}, которая занимается классифицированием чисел типа double. Можно почитать [эту](https://steve.hollasch.net/cgindex/coding/ieeefloat.html) статью которую мы разбирали на семинаре. Так же на youtube я [выложил](https://youtu.be/SoacNd8tV9I) видео с комментариями по задаче.
+
+##### Стоимость:
+Задача стоит 3 балла.
+
---
+++ classify double/solution.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+
+/**
+ * Library-level functions.
+ * You should use them in the main sections.
+ */
+
+uint64_t convertToUint64 (double number) {
+	return *((uint64_t *)(&number));
+}
+
+bool getBit (const uint64_t number, const uint8_t index) {
+	/// Your code here...
+}
+
+
+/**
+ * Checkers here:
+ */
+
+bool checkForPlusZero (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForMinusZero (uint64_t number) {
+	return number == 0x8000000000000000;
+}
+
+bool checkForPlusInf (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForMinusInf (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForPlusNormal (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForMinusNormal (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForPlusDenormal (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForMinusDenormal (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForSignalingNan (uint64_t number) {
+    /// Your code here.
+}
+
+bool checkForQuietNan (uint64_t number) {
+    /// Your code here.
+}
+
+
+void classify (double number) {
+	if (checkForPlusZero(convertToUint64(number))) {
+	    printf("Plus zero\n");
+	}
+
+	else if (checkForMinusZero(convertToUint64(number))) {
+        printf("Minus zero\n");
+	}
+
+	else if (checkForPlusInf(convertToUint64(number))) {
+        printf("Plus inf\n");
+	}
+
+	else if (checkForMinusInf(convertToUint64(number))) {
+        printf("Minus inf\n");
+	}
+
+	else if (checkForPlusNormal(convertToUint64(number))) {
+        printf("Plus regular\n");
+	}
+
+	else if (checkForPlusNormal(convertToUint64(number))) {
+        printf("Minus regular\n");
+	}
+
+	else if (checkForPlusDenormal(convertToUint64(number))) {
+        printf("Plus Denormal\n");
+	}
+
+	else if (checkForMinusDenormal(convertToUint64(number))) {
+        printf("Minus Denormal\n");
+	}
+
+	else if (checkForSignalingNan(convertToUint64(number))) {
+        printf("Signailing NaN\n");
+	}
+
+	else if (checkForQuietNan(convertToUint64(number))) {
+        printf("Quiet NaN\n");
+	}
+
+	else {
+        printf("Error.\n");
+	}
+}
+
