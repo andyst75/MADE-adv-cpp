880526cf5b431d81da32a81b9a1a493ed7ab9ff7
Konstantin Leladze <leladze.kg@phystech.edu>
2020-09-15 13:52:35 +0300

    fixed indentations

--- classify double/solution.c
+++ classify double/solution.c
@@ -11,11 +11,11 @@
  */
 
 uint64_t convertToUint64 (double number) {
-	return *((uint64_t *)(&number));
+    return *((uint64_t *)(&number));
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-	/// Your code here...
+    /// Your code here...
 }
 
 
@@ -28,7 +28,7 @@ bool checkForPlusZero (uint64_t number) {
 }
 
 bool checkForMinusZero (uint64_t number) {
-	return number == 0x8000000000000000;
+    return number == 0x8000000000000000;
 }
 
 bool checkForPlusInf (uint64_t number) {
@@ -65,48 +65,48 @@ bool checkForQuietNan (uint64_t number) {
 
 
 void classify (double number) {
-	if (checkForPlusZero(convertToUint64(number))) {
-	    printf("Plus zero\n");
-	}
+    if (checkForPlusZero(convertToUint64(number))) {
+        printf("Plus zero\n");
+    }
 
-	else if (checkForMinusZero(convertToUint64(number))) {
+    else if (checkForMinusZero(convertToUint64(number))) {
         printf("Minus zero\n");
-	}
+    }
 
-	else if (checkForPlusInf(convertToUint64(number))) {
+    else if (checkForPlusInf(convertToUint64(number))) {
         printf("Plus inf\n");
-	}
+    }
 
-	else if (checkForMinusInf(convertToUint64(number))) {
+    else if (checkForMinusInf(convertToUint64(number))) {
         printf("Minus inf\n");
-	}
+    }
 
-	else if (checkForPlusNormal(convertToUint64(number))) {
+    else if (checkForPlusNormal(convertToUint64(number))) {
         printf("Plus regular\n");
-	}
+    }
 
-	else if (checkForPlusNormal(convertToUint64(number))) {
+    else if (checkForPlusNormal(convertToUint64(number))) {
         printf("Minus regular\n");
-	}
+    }
 
-	else if (checkForPlusDenormal(convertToUint64(number))) {
+    else if (checkForPlusDenormal(convertToUint64(number))) {
         printf("Plus Denormal\n");
-	}
+    }
 
-	else if (checkForMinusDenormal(convertToUint64(number))) {
+    else if (checkForMinusDenormal(convertToUint64(number))) {
         printf("Minus Denormal\n");
-	}
+    }
 
-	else if (checkForSignalingNan(convertToUint64(number))) {
+    else if (checkForSignalingNan(convertToUint64(number))) {
         printf("Signailing NaN\n");
-	}
+    }
 
-	else if (checkForQuietNan(convertToUint64(number))) {
+    else if (checkForQuietNan(convertToUint64(number))) {
         printf("Quiet NaN\n");
-	}
+    }
 
-	else {
+    else {
         printf("Error.\n");
-	}
+    }
 }
 
