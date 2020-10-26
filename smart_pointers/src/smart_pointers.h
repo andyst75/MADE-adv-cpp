8b9d041b96d70b3f2b29d6a564696f02274fe390
Roman Gurov <roman.gurov0@gmail.com>
2020-10-26 21:51:49 +0300

    Add smart pointers

---
+++ smart_pointers/src/smart_pointers.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+namespace task {
+
+template <class T>
+class UniquePtr {
+    // Your code goes here...
+};
+
+template <class T>
+class SharedPtr {
+    // Your code goes here...
+};
+
+template <class T>
+class WeakPtr {
+    // Your code goes here...
+};
+
+
+// Your template function definitions may go here...
+
+}  // namespace task
+
+
+#include "smart_pointers.tpp"
---
+++ smart_pointers/src/smart_pointers.tpp
@@ -0,0 +1,5 @@
+namespace task {
+
+// Your template function definitions go here...
+
+}
