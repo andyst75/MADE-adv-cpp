1fe97776f87cf0a93a7b1d6ff7c140df46dbafc3
andyst@mail.ru <AL88kvfWE4U5CVL>
2020-11-21 02:11:19 +0500

    hw7

--- hw_7_meanwhile_in_the_seventh_galaxy/third_variant/src/main.cpp
+++ hw_7_meanwhile_in_the_seventh_galaxy/third_variant/src/main.cpp
@@ -5,18 +5,28 @@
 
 
 int main () {
-    using graph =
-        Graph<
-            Vertex<0, Edge<0, 1>>,
-            Vertex<1, Edge<1, 2>, Edge<1, 5>>,
-            Vertex<2, Edge<2, 3>, Edge<2, 0>>,
-            Vertex<3, Edge<3, 4>>,
-            Vertex<4, Edge<4, 1>>,
-            Vertex<5, Edge<5, 4>>
-        >;
 
-    std::cout << "Path exists: " <<
-        (PathExists<graph, 5, 0>::value ? "true" : "false") << std::endl;
+  using graph =
+  Graph<
+  Vertex<0, Edge<0, 1>>,
+  Vertex<1, Edge<1, 2>, Edge<1, 5>>,
+  Vertex<2, Edge<2, 3>, Edge<2, 0>>,
+  Vertex<3, Edge<3, 4>>,
+  Vertex<4, Edge<4, 1>>,
+  Vertex<5, Edge<5, 4>, Edge<5, 6>>,
+  Vertex<6, Edge<6, 7>>,
+  Vertex<7, Edge<7, 8>>,
+  Vertex<8, Edge<8, 6>>
+  >;
+
+  std::cout << "Path exists (T): "
+      << (PathExists<graph, 5, 0>::value ? "true" : "false") << std::endl;
+
+  std::cout << "Path exists (F): "
+      << (PathExists<graph, 7, 5>::value ? "true" : "false") << std::endl;
+
+  std::cout << "Path exists (T): "
+      << (PathExists<graph, 0, 0>::value ? "true" : "false") << std::endl;
 
     return 0;
 }
